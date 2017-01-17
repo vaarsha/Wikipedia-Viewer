c375f15fef6620407509cc7e3992cad7fdf6e4f0
Varsha Rao <varshar186@gmail.com>
2017-01-17 19:07:46 +0530

    testing

--- js/index.js
+++ js/index.js
@@ -6,3 +6,11 @@ function search() {
 	var vl = document.getElementById('ipt').value;
 	alert(vl);
 }
+
+$(document).ready(function(){
+$("#ipt").keypress(function(e) {
+	if(e.which == 13) {
+		alert('You pressed enter!');
+	}
+});
+});
