98cf409f237acc13ed5a83756783c4f20eab7a4a
Varsha Rao <varshar186@gmail.com>
2017-01-19 00:31:20 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,15 +2,23 @@ function random() {
 	window.open('https://en.wikipedia.org/wiki/Special:Random');
 }
 
+var tags = [];
 function search() {
-	var vl = document.getElementById('ipt').value;
-	alert(vl);
+	var vl = $('#ipt').val();
+	$.get('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + vl + '&limit=5&namespace=0&format=json', function( data ) {
+		tags = data[1];
+		console.log(data[1]);
+	}, "jsonp" );
 }
+
 
 $(document).ready(function(){
 $("#ipt").keypress(function(e) {
 	if(e.which == 13) {
-		alert('You pressed enter!');
+		e.preventDefault();
+		search();
 	}
 });
 });
+
+
