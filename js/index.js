da8dda35943b56eade16a84ce5b9c508e5c0fc7e
Varsha Rao <varshar186@gmail.com>
2017-01-20 07:09:36 +0530

    Added click method for search button.

--- js/index.js
+++ js/index.js
@@ -8,17 +8,23 @@ function search() {
 	$.get('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + vl + '&limit=5&namespace=0&format=json', function( data ) {
 		tags = data[1];
 		console.log(data[1]);
+		document.getElementById("output").innerHTML="hello";
 	}, "jsonp" );
 }
 
-
 $(document).ready(function(){
-$("#ipt").keypress(function(e) {
-	if(e.which == 13) {
-		e.preventDefault();
+	//On pressing enter key
+	$("#ipt").keypress(function(e) {
+		if(e.which == 13) {
+			e.preventDefault();
+			search();
+		}
+	});
+	//on click of search button
+	$("#sbm1").click(function(){
 		search();
-	}
-});
+		return false;
+	});
 });
 
 
