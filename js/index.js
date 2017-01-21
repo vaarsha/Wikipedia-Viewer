21bf057da150f5911a9bf8c09d4adfdeec3c5e98
vaarsha <varshar186@gmail.com>
2017-01-21 23:29:29 +0530

    Removed variable tags.

--- js/index.js
+++ js/index.js
@@ -2,11 +2,9 @@ function random() {
 	window.open('https://en.wikipedia.org/wiki/Special:Random');
 }
 
-var tags = [];
 function search() {
 	var vl = $('#ipt').val();
 	$.get('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + vl + '&limit=5&namespace=0&format=json', function( data ) {
-		tags = data[1];
 		console.log(data[1]);
 	}, "jsonp" );
 }
