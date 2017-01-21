d65555fc1a1b2daeddc382baa205d3dda0363c80
vaarsha <varshar186@gmail.com>
2017-01-21 23:32:05 +0530

    Added function DisplayResults.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 function random() {
 	window.open('https://en.wikipedia.org/wiki/Special:Random');
 }
-
+function DisplayResult(data) {
+console.log(data);
+    $("#rs").html(""); // Clear
+  var result = "";
+ var name,link,cont;
+  for(var i =0;i<5;i++){
+  name = data[1][i];
+    cont = data[2][i];
+    link = data[3][i];
+  result ='<a href="'+link+'" target="_blank" id="lk">';
+    result += "<h3>"+name+"</h3>";
+    result+='<p id="pid">'+cont+'</p>';
+  result +='</a>';
+  $("#rs").append(result);
+  }
+}
 function search() {
 	var vl = $('#ipt').val();
 	$.get('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + vl + '&limit=5&namespace=0&format=json', function( data ) {
