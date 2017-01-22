630db2e56f24da0bffd4dc6e3f5ee2038c2dbb83
Varsha Rao <varshar186@gmail.com>
2017-01-22 16:17:02 +0530

    Added append method.

--- js/index.js
+++ js/index.js
@@ -2,20 +2,21 @@ function random() {
 	window.open('https://en.wikipedia.org/wiki/Special:Random');
 }
 function DisplayResult(data) {
-console.log(data);
-    $("#rs").html(""); // Clear
-  var result = "";
- var name,link,cont;
-  for(var i =0;i<5;i++){
-  name = data[1][i];
-    cont = data[2][i];
-    link = data[3][i];
-  result ='<a href="'+link+'" target="_blank" id="lk">';
-    result += "<h3>"+name+"</h3>";
-    result+='<p id="pid">'+cont+'</p>';
-  result +='</a>';
-  $("#rs").append(result);
-  }
+	console.log(data);
+	$("#rs").html(""); // Clear
+	var result = "";
+	var name,link,cont;
+	for(var i =0;i<5;i++){
+		name = data[1][i];
+		cont = data[2][i];
+		link = data[3][i];
+		result ='<a href="'+link+'" target="_blank" id="lk">';
+		result += "<h3>"+name+"</h3>";
+		result+='<p id="pid">'+cont+'</p>';
+		result +='</a>';
+		$("#rs").append(result);
+	}
+	$("footer").before("<hr>");
 }
 function search() {
 	var vl = $('#ipt').val();
